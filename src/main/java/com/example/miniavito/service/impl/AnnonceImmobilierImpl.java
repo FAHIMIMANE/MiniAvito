d98fb4b8ebeefe1c9d9bc05fbd30b76baffd265e
Kaoutar <kaoutarmghar@gmail.com>
2022-04-07 21:48:52 +0000

    service

--- src/main/java/com/example/miniavito/service/impl/AnnonceImmobilierImpl.java
+++ src/main/java/com/example/miniavito/service/impl/AnnonceImmobilierImpl.java
@@ -3,7 +3,9 @@ package com.example.miniavito.service.impl;
 import com.example.miniavito.dao.AnnonceImmobilierDao;
 import com.example.miniavito.service.facade.AnnoceImmobilierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AnnonceImmobilierImpl implements AnnoceImmobilierService {
 
     @Autowired
