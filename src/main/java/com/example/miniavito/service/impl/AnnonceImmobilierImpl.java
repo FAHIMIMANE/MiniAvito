3422840c6a3fae98ad29065577dd3241d677bd3e
IMANE <imanefahim2@gmail.com>
2022-04-07 22:35:28 +0000

    initial commit 2

--- src/main/java/com/example/miniavito/service/impl/AnnonceImmobilierImpl.java
+++ src/main/java/com/example/miniavito/service/impl/AnnonceImmobilierImpl.java
@@ -2,6 +2,7 @@ package com.example.miniavito.service.impl;
 
 import com.example.miniavito.dao.AnnonceImmobilierDao;
 import com.example.miniavito.service.facade.AnnoceImmobilierService;
+import com.example.miniavito.service.facade.TypeImmobilierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +11,9 @@ public class AnnonceImmobilierImpl implements AnnoceImmobilierService {
 
     @Autowired
     private AnnonceImmobilierDao annonceImmobilierDao;
+    @Autowired
+  private TypeImmobilierService typeImmobilierService;
+
+
+
 }
