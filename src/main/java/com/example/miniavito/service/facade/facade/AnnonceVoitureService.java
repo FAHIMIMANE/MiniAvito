36bc5515e72215ee3adf5dc9e31b51a73885cea3
Kaoutar <kaoutarmghar@gmail.com>
2022-04-07 22:57:38 +0000

    service

--- src/main/java/com/example/miniavito/service/facade/facade/AnnonceVoitureService.java
+++ src/main/java/com/example/miniavito/service/facade/facade/AnnonceVoitureService.java
@@ -1,4 +1,10 @@
 package com.example.miniavito.service.facade.facade;
 
+import com.example.miniavito.bean.AnnonceVoiture;
+
+import java.util.List;
+
 public interface AnnonceVoitureService {
+
+    List<AnnonceVoiture> findByRefCarburant(String refCarburant);
 }
