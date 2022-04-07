06d3e2268717e5af50c76c0a2f4ab5b01e8419d7
skodev-sudo <60196804+skodev-sudo@users.noreply.github.com>
2022-04-08 01:54:31 +0200

    Annonceheuresupdao changed by sko :) !

--- src/main/java/com/example/miniavito/service/impl/AnnonceHeureSuplementaireImpl.java
+++ src/main/java/com/example/miniavito/service/impl/AnnonceHeureSuplementaireImpl.java
@@ -1,12 +1,34 @@
 package com.example.miniavito.service.impl;
 
+import com.example.miniavito.bean.AnnonceHeureSuplementaire;
 import com.example.miniavito.dao.AnnonceHeureSuplementaireDao;
 import com.example.miniavito.service.facade.AnnonceHeureSuplementaireService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AnnonceHeureSuplementaireImpl implements AnnonceHeureSuplementaireService {
     @Autowired
     private AnnonceHeureSuplementaireDao annonceHeureSuplementaireDao;
+
+    public AnnonceHeureSuplementaire findByRef(String ref) {
+        return annonceHeureSuplementaireDao.findByRef(ref);
+    }
+
+    public List<AnnonceHeureSuplementaire> findByMontantLessThanEqual(double montant){
+        return annonceHeureSuplementaireDao.findByMontantLessThanEqual(montant);
+    }
+    public List<AnnonceHeureSuplementaire> findByMontantGreaterThanEqual(double montant){
+        return annonceHeureSuplementaireDao.findByMontantGreaterThanEqual(montant);
+    }
+
+    public List<AnnonceHeureSuplementaire> findByRefMatiere(String refMatiere){
+        return annonceHeureSuplementaireDao.findByRefMatiere(refMatiere);
+    }
+
+    public List<AnnonceHeureSuplementaire> findByJourDisponibilite(String jourDisponibilite){
+        return annonceHeureSuplementaireDao.findByJourDisponibilite(jourDisponibilite);
+    }
 }
