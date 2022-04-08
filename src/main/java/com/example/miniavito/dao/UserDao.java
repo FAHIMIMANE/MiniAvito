acd40b41f4a46aed1ed031c986805b94840d3551
MINAidahmed <mina.idahmed@gmail.com>
2022-04-08 10:18:03 +0200

    YFGHJGHU

--- src/main/java/com/example/miniavito/dao/UserDao.java
+++ src/main/java/com/example/miniavito/dao/UserDao.java
@@ -4,7 +4,12 @@ import com.example.miniavito.bean.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserDao extends JpaRepository<User,Long> {
+    User findByLogin(String login);
     User findByRef(String ref);
+
+    List<User> findByProf(boolean prof);
 }
--- src/main/java/com/example/miniavito/service/facade/UserService.java
+++ src/main/java/com/example/miniavito/service/facade/UserService.java
@@ -2,6 +2,19 @@ package com.example.miniavito.service.facade;
 
 import com.example.miniavito.bean.User;
 
+import java.util.List;
+
 public interface UserService {
-    User findByRef(String ref);
+    public User findByLogin(String login);
+
+    public User findByRef(String login);
+
+
+
+    public List<User> findByProf(boolean prof);
+
+
+    public int save(User user) ;
+
+
 }
